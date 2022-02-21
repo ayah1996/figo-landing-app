3196f0cdac8901111793e89282f29e6ed69f9235
ayah1996 <ayah.o.ezzeddine@gmail.com>
2022-02-22 01:43:21 +0200

    fix history responsive

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
     <>
       <Header />
       <AboutSection />
+      <HistorySection />
       {/*<ReasonSection />
       <BrowseSection />
-      <HistorySection />
       <TestimonialsSection />
       <NewsSection />
       <SubscribeSection />
--- src/Components/HistorySection/History.js
+++ src/Components/HistorySection/History.js
@@ -7,10 +7,10 @@ const HistorySection = () => {
   return (
     <>
       <Container>
-        <Row className="gx-5 text-center text-xl-start mb-section">
+        <Row className="gx-5 mb-section">
           <Col
-            xl={6}
-            className="order-2 order-xxl-1 align-self-center  mt-5 mt-xl-0"
+            lg={6}
+            className="order-2 order-lg-1  text-center text-lg-start align-self-center mt-5 mt-lg-0"
           >
             <h5 className="sub-heading">welcome</h5>
             <h3 className="heading">
@@ -26,11 +26,15 @@ const HistorySection = () => {
               prospective internal or "organic" sources. Synergistically mesh
               quality quality vectors for one-to-one leadership.
             </p>
-            <div className="aboutBtn mx-auto mx-xl-0">
+            <div className="aboutBtn mx-auto mx-lg-0">
               <CustomBtn text={"Explore Our Story"} />
             </div>
           </Col>
-          <Col xl={6} className="order-1 order-xxl-2 text-xl-end mt-5 mt-xl-0">
+          <Col
+            sm={8}
+            md={6}
+            className="order-1 order-xxl-2 mx-auto mx-lg-0 text-xl-end mt-5 mt-lg-0"
+          >
             <img src={HistoryImg} class="img-fluid" alt="figo" />
           </Col>
         </Row>
