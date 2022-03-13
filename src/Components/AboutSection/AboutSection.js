dec7352e6eaa2f55f905c0b857e9b28fbc731949
ayah1996 <ayah.o.ezzeddine@gmail.com>
2022-03-13 16:43:10 +0200

    fix

--- src/Components/AboutSection/AboutSection.js
+++ src/Components/AboutSection/AboutSection.js
@@ -11,13 +11,7 @@ const AboutSection = () => {
       <Container>
         <Row className="gx-5 text-center text-lg-start ">
           <Col sm={8} md={6} className="mx-auto mx-lg-0">
-            <img
-              src="/Assets/welcome.webp"
-              rel="preload"
-              as="image"
-              className="img-fluid"
-              alt="figo"
-            />
+            <img src="/Assets/welcome.webp" className="img-fluid" alt="figo" />
           </Col>
           <Col lg={6} className="align-self-center mt-5 mt-lg-0">
             <h5 className="sub-heading">welcome</h5>
