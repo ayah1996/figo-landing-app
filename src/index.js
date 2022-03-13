82991a798d28b2156b2cf1ee171a1ae14b430801
ayah1996 <ayah.o.ezzeddine@gmail.com>
2022-03-13 16:12:03 +0200

    purgecss

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./Styles/App.scss";
 import App from "./App";
+import "./Styles/App.scss";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
