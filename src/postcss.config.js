cf1e8c9e891fa83b78dcb1c3a7fe22051b180c9e
ayah1996 <ayah.o.ezzeddine@gmail.com>
2022-03-14 04:45:49 +0200

    remove pizza hover from small screen header section

--- src/postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    purgecss({
-      content: ["./src/**/*.js"],
-      // Treat every word in the bundle as a CSS selector
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-    }),
-  ],
-};
